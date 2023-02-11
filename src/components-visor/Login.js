4fa1a9df2462773fbc6c92e549425b454349352d
Andres <anm2367l@gmail.com>
2023-02-11 00:47:48 -0500

    Funcionamiento login y creacion parcial entorno de trabajo

--- src/components-visor/Login.js
+++ src/components-visor/Login.js
@@ -57,70 +57,74 @@ const Login = () => {
 
 
   }, [loggedStatus, estado]);
-
+  //container p-3 align-items-center justify-content-center vh-100 vw-100 
+  //text-center border bg-light rounded-3
   //modifica los cambios al ingresar los datos cedula y contraseña
   return (
-    <div className="text-center border bg-light rounded-3 " id="login-div-login">
-      <main className="form-signin mt-3 mb-1 me-5 ms-5">
-        <form onSubmit={handleSumit} className='text-center'>
-          <img
-            className="mb-4 center"
-            src="https://docs.google.com/uc?export=download&id=1K75xLK6LzMSvMvcvDRVtABqRz8AwgEb9"
-            alt=""
-            width="50%"
-            height="50%"
-          />
-          <p className="fs-3 text-start fw-normal mb-1">
-            Farming Assistant <br />
-          </p>
-          <p className="fs-6 text-start fw-light">  Bienvenido, ingrese sus datos</p>
-          <div className="form-floating">
-            <input
-              name="email"
-              type="text"
-              className="form-control"
-              id="email-id"
-              placeholder="@Email"
-              onChange={handleInputChange}
-              value={email}
-            />
-            <label htmlFor="correo-id"><p className="font-monospace">Correo electronico</p></label>
-          </div>
-          <div className="form-floating">
-            <input
-              name="password"
-              type="password"
-              className="form-control"
-              id="contraseña-id"
-              placeholder="Contraseña"
-              onChange={handleInputChange}
-              value={password}
-            />
-            <label htmlFor="contraseña-id"><p className="font-monospace">Contraseña</p></label>
-          </div>
-          <div className="row pt-3">
-            <div className="col">
-              <button className="w-100 btn btn-lg btn-primary" type="submit">
-                Ingersar
-              </button>
-            </div>
-          </div>
-          <div className="mt-1 bg-body rounded-3">
+    <div className="row ms-5 ps-5 pe-5 mt-5 border justify-content-md-center" id="login-div-login">
+      
+      <div className="border col-sm text-center border bg-light rounded-3">
+        <main className="form-signin border  mt-3 mb-1 me-5 ms-5">
+          <form onSubmit={handleSumit} className='text-center'>
             <img
-              className="mt-3 center"
+              className="mb-4 center"
               src="https://docs.google.com/uc?export=download&id=1K75xLK6LzMSvMvcvDRVtABqRz8AwgEb9"
               alt=""
-              width="40%"
-              height="40%"
+              width="50%"
+              height="50%"
             />
-            <p className=" mb-3 fs-6  fw-light">
-              &copy; Universidad Industrial de Santander <br />
-              Ingenieria de Sistemas <br />
-              2023
+            <p className="fs-3 text-start fw-normal mb-1">
+              Farming Assistant <br />
             </p>
-          </div>
-        </form>
-      </main>
+            <p className="fs-6 text-start fw-light">  Bienvenido, ingrese sus datos</p>
+            <div className="form-floating">
+              <input
+                name="email"
+                type="text"
+                className="form-control"
+                id="email-id"
+                placeholder="@Email"
+                onChange={handleInputChange}
+                value={email}
+              />
+              <label htmlFor="correo-id"><p className="font-monospace">Correo electronico</p></label>
+            </div>
+            <div className="form-floating">
+              <input
+                name="password"
+                type="password"
+                className="form-control"
+                id="contraseña-id"
+                placeholder="Contraseña"
+                onChange={handleInputChange}
+                value={password}
+              />
+              <label htmlFor="contraseña-id"><p className="font-monospace">Contraseña</p></label>
+            </div>
+            <div className="row pt-3">
+              <div className="col">
+                <button className="w-100 btn btn-lg btn-primary" type="submit">
+                  Ingersar
+                </button>
+              </div>
+            </div>
+            <div className="mt-1 bg-body rounded-3">
+              <img
+                className="mt-3 center"
+                src="https://docs.google.com/uc?export=download&id=1K75xLK6LzMSvMvcvDRVtABqRz8AwgEb9"
+                alt=""
+                width="40%"
+                height="40%"
+              />
+              <p className=" mb-3 fs-6  fw-light">
+                &copy; Universidad Industrial de Santander <br />
+                Ingenieria de Sistemas <br />
+                2023
+              </p>
+            </div>
+          </form>
+        </main>
+      </div>
     </div>
   );
 };
